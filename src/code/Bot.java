c182d3e3293c6cfaab66b0b91adf4328b3166aec
KameriiJ <jenny.sirarat@gmail.com>
2017-11-19 11:01:55 +0700

    fixed bot code

--- src/code/Bot.java
+++ src/code/Bot.java
@@ -1,6 +1,5 @@
 package code;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.Random;
@@ -9,18 +8,18 @@ public class Bot extends Rectangle{
 	
 	private static final long serialVersionUID = 1L;
 	
-	private int random = 0,smart = 1;
+	private int random = 0,smart = 1,find_path = 2;
 	private int state = random;
 	private int right = 0,left = 1,up = 2,down = 3;
 	private int dir = -1;
+	private String lastDir = "";
 	
 	public Random randomGen;
 	
-	
 	private int time = 0;
-	private int targetTime = 60*2;
+	private int targetTime = 60*1;
 	
-	private int speedE = 3;
+	private int speedE = 4;
 
 	public Bot(int x ,int y){
 		randomGen = new Random();
@@ -44,156 +43,112 @@ public class Bot extends Rectangle{
 				 if(canMove(x+speedE, y)) {
 					 x+=speedE;
 					 move = true;
+					 lastDir = "right";
 				 }
 			 }
 			 if(x > Game2.player.x) { 
 				 if(canMove(x-speedE, y)) { 
 					 x-=speedE;
 					 move = true;
+					 lastDir = "left";
 				 }
 			 }
-			 if(y < Game2.player.y) { 
+			 if(y < Game2.player.y) {
 				 if(canMove(x, y+speedE)) { 
 					 y+=speedE;
 					 move = true;
+					 lastDir = "down";
 				 }
 			 }
 			 if(y > Game2.player.y) { 
 				 if(canMove(x, y-speedE)) { 
 					 y-=speedE;
 					 move = true;
+					 lastDir = "up";
 				 }
 			 }
-			 
 			if(x == Game2.player.x && y == Game2.player.y) move = true;
+			if(!move) {
+				state = find_path;
+			}
 			time++;
-			if(time == 60*10) {
+			if(time == 60*7) {
 				state = random;
 				time = 0;
 			}
 		}
+		else if(state == find_path) {
+			findPath();
+		}
 	}
 	
 	public void findPath() {
-		if(x < Game2.player.x) { 
-			 if(canMove(x+speedE, y)) {
-				 x+=speedE;
-			 }
-			 else if(!canMove(x+speedE, y)){
-				 if(y < Game2.player.y){
-					 if(canMove(x, y+speedE)) {
-						y+=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(canMove(x, y-speedE))){
-						 y-=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(!canMove(x, y-speedE))){
-						 x-=speedE;
-					 }
-				 }
-				 else if(y > Game2.player.y) {
-					 if(canMove(x, y-speedE)) {
-							y-=speedE;
-						 }
-					 else if((!canMove(x, y-speedE))&&(canMove(x, y+speedE))){
-						 y+=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(!canMove(x, y-speedE))){
-						 x-=speedE;
-					 }
-				 }
-			 }
-		 }
-		 
-		else if(x > Game2.player.x) { 
-			 if(canMove(x-speedE, y)) {
-				 x-=speedE;
-			 }
-			 else if(!canMove(x+speedE, y)){
-				 if(y < Game2.player.y){
-					 if(canMove(x, y+speedE)) {
-						y+=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(canMove(x, y-speedE))){
-						 y-=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(!canMove(x, y-speedE))){
-						 x+=speedE;
-					 }
-				 }
-				 else if(y > Game2.player.y) {
-					 if(canMove(x, y-speedE)) {
-							y-=speedE;
-						 }
-					 else if((!canMove(x, y-speedE))&&(canMove(x, y+speedE))){
-						 y+=speedE;
-					 }
-					 else if((!canMove(x, y+speedE))&&(!canMove(x, y-speedE))){
-						 x+=speedE;
-					 }
-				 }
-			 }
-		 }
-		 
-		else if(y < Game2.player.y) { 
-			 if(canMove(x, y+speedE)) {
-				 y+=speedE;
-			 }
-			 else if(!canMove(x, y+speedE)){
-				 if(x < Game2.player.y){
-					 if(canMove(x+speedE, y)) {
-						x+=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(canMove(x-speedE, y))){
-						 x-=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(!canMove(x-speedE, y))){
-						 y-=speedE;
-					 }
-				 }
-				 else if(x > Game2.player.y) {
-					 if(canMove(x-speedE, y)) {
-							x-=speedE;
-						 }
-					 else if((!canMove(x-speedE, y))&&(canMove(x+speedE, y))){
-						 x+=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(!canMove(x-speedE, y))){
-						 y-=speedE;
-					 }
-				 }
-			 }
-		 }
-		 
-		else if(y > Game2.player.y) { 
-			 if(canMove(x, y-speedE)) {
-				 y-=speedE;
-			 }
-			 else if(!canMove(x, y-speedE)){
-				 if(x < Game2.player.y){
-					 if(canMove(x+speedE, y)) {
-						x+=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(canMove(x-speedE, y))){
-						 x-=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(!canMove(x-speedE, y))){
-						 y+=speedE;
-					 }
-				 }
-				 else if(x > Game2.player.y) {
-					 if(canMove(x-speedE, y)) {
-							x-=speedE;
-						 }
-					 else if((!canMove(x-speedE, y))&&(canMove(x+speedE, y))){
-						 x+=speedE;
-					 }
-					 else if((!canMove(x+speedE, y))&&(!canMove(x-speedE, y))){
-						 y+=speedE;
-					 }
-				 }
-			 }
-		 }
+		if(lastDir.equals("right")) {
+			if(y < Game2.player.y) {
+				if(canMove(x, y+speedE)) {
+					y+=speedE;
+					state = smart;
+				}
+			}else {
+				if(canMove(x, y-speedE)) {
+					y-=speedE;
+					state = smart;
+				}
+			}
+			if(canMove(x+speedE, y)) x+=speedE;
+		}
+		
+		else if(lastDir.equals("left")) {
+			if(y < Game2.player.y) {
+				if(canMove(x, y+speedE)) {
+					y+=speedE;
+					state = smart;
+				}
+			}else {
+				if(canMove(x, y-speedE)) {
+					y-=speedE;
+					state = smart;
+				}
+			}
+			if(canMove(x-speedE, y)) x-=speedE;
+		}
+		
+		else if(lastDir.equals("up")) {
+			if(x < Game2.player.x) {
+				if(canMove(x+speedE, y)) {
+					x+=speedE;
+					state = smart;
+				}
+			}else {
+				if(canMove(x-speedE, y)) {
+					x-=speedE;
+					state = smart;
+				}
+			}
+			if(canMove(x, y-speedE)) y-=speedE;
+		}
+		
+		else if(lastDir.equals("down")) {
+			if(x < Game2.player.x) {
+				if(canMove(x+speedE, y)) {
+					x+=speedE;
+					state = smart;
+				}
+			}else {
+				if(canMove(x-speedE, y)) {
+					x-=speedE;
+					state = smart;
+				}
+				
+			}
+			if(canMove(x, y+speedE)) y+=speedE;
+		}
+	
+		time++;
+		if(time == 60*7) {
+			state = random;
+			time = 0;
+		}
 	}
 	
 	public void checkMove(){
