c6cabb58466a2aa1f818a842f0e7907c98e9ee63
KameriiJ <jenny.sirarat@gmail.com>
2017-11-23 20:55:59 +0700

    add time images

--- src/GUI/ModeSelected.java
+++ src/GUI/ModeSelected.java
@@ -1,21 +1,20 @@
-package GUI;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package GUI;
 
 /**
  *
  * @author Smarty
  */
-public class ModeSelected extends javax.swing.JFrame {
+public class ModeSelected1 extends javax.swing.JFrame {
 
     /**
-     * Creates new form ModeSelected
+     * Creates new form ModeSelected1
      */
-    public ModeSelected() {
+    public ModeSelected1() {
         initComponents();
          this.setLocationRelativeTo(null);
     }
@@ -31,18 +30,41 @@ public class ModeSelected extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jButton8 = new javax.swing.JButton();
         singleplayer = new javax.swing.JButton();
         twoplayer = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(800, 640));
-        setSize(new java.awt.Dimension(800, 640));
 
+        jPanel1.setPreferredSize(new java.awt.Dimension(800, 640));
         jPanel1.setSize(new java.awt.Dimension(800, 640));
+        jPanel1.setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("VitaminTabletE", 0, 36)); // NOI18N
         jLabel1.setText("Please Select Mode");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(162, 68, 501, 36);
+
+        singleplayer.setFont(new java.awt.Font("Marker Felt", 0, 36)); // NOI18N
+        singleplayer.setText("1 Player");
+        singleplayer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                singleplayerActionPerformed(evt);
+            }
+        });
+        jPanel1.add(singleplayer);
+        singleplayer.setBounds(160, 330, 159, 48);
+
+        twoplayer.setFont(new java.awt.Font("Marker Felt", 0, 36)); // NOI18N
+        twoplayer.setText("2 Player");
+        twoplayer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                twoplayerActionPerformed(evt);
+            }
+        });
+        jPanel1.add(twoplayer);
+        twoplayer.setBounds(480, 330, 159, 48);
 
         jButton8.setFont(new java.awt.Font("Marker Felt", 0, 36)); // NOI18N
         jButton8.setText("Back");
@@ -52,86 +74,45 @@ public class ModeSelected extends javax.swing.JFrame {
                 jButton8ActionPerformed(evt);
             }
         });
+        jPanel1.add(jButton8);
+        jButton8.setBounds(41, 557, 150, 50);
 
-        singleplayer.setFont(new java.awt.Font("Marker Felt", 0, 36)); // NOI18N
-        singleplayer.setText("1 Player");
-        singleplayer.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                singleplayerActionPerformed(evt);
-            }
-        });
-
-        twoplayer.setFont(new java.awt.Font("Marker Felt", 0, 36)); // NOI18N
-        twoplayer.setText("2 Player");
-        twoplayer.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                twoplayerActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
-                        .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(145, 145, 145)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(singleplayer)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(twoplayer))
-                            .addComponent(jLabel1))))
-                .addContainerGap(154, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(90, 90, 90)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 281, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(singleplayer)
-                        .addGap(91, 91, 91))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(twoplayer)
-                        .addGap(102, 102, 102)))
-                .addComponent(jButton8, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(33, 33, 33))
-        );
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/Modeselected.gif"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(0, 0, 800, 640);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void singleplayerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_singleplayerActionPerformed
-        new Difficultyselected().setVisible(true);
+        new Difficultyselected1().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_singleplayerActionPerformed
+
+    private void twoplayerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_twoplayerActionPerformed
+        TwoPlayerMode.Game.main();
+    }//GEN-LAST:event_twoplayerActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
         new Items().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton8ActionPerformed
-
-    private void twoplayerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_twoplayerActionPerformed
-        TwoPlayerMode.Game.main();
-    }//GEN-LAST:event_twoplayerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -150,20 +131,20 @@ public class ModeSelected extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ModeSelected.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModeSelected1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ModeSelected.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModeSelected1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ModeSelected.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModeSelected1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ModeSelected.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ModeSelected1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ModeSelected().setVisible(true);
+                new ModeSelected1().setVisible(true);
             }
         });
     }
@@ -171,6 +152,7 @@ public class ModeSelected extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton8;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton singleplayer;
     private javax.swing.JButton twoplayer;
