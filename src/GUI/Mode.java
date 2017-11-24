55fc3304e39628667fdc95e02a08e8b4cb82df0a
KameriiJ <jenny.sirarat@gmail.com>
2017-11-24 19:21:55 +0700

    add Mode class

---
+++ src/GUI/Mode.java
@@ -0,0 +1,18 @@
+package GUI;
+
+public class Mode {
+	private String mode;
+	
+	public Mode(String mode) {
+		this.mode = mode;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+
+}
